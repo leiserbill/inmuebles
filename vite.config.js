ef0e6fe67238f1433215c2184037f3822e5e7e5e
leiserbill <leiserbill@hotmail.com>
2025-03-01 23:17:00 -0600

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
      
-        laravel({
-            input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
-                'resources/js/mapa.js',
-                'resources/js/mapainicio.js'
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js', // Update the entry module here
+        ]),
     ],
 });
